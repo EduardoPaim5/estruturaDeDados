a16c5355d49c003cb937f98b9bf56d18b9ec6a24
EduardoPaim <epaimmmv@gmail.com>
2024-04-04 15:22:24 -0300

    Segunda Tarefa,TAD Vetor para armazenar Alunos.

---
+++ src/Atividade2/Principal.java
@@ -0,0 +1,38 @@
+package Atividade2;
+
+public class Principal {
+    public static void main(String[] args) {
+        Vetor vetAlunos = new Vetor(5);
+
+        Aluno a1 = new Aluno("Nikola Tesla", 15, 10.0);
+
+        Aluno a2 = new Aluno("Isabela", 17, 6.6);
+
+        Aluno a3 = new Aluno("Vinicius", 12, 7.0);
+
+        Aluno a4 = new Aluno("Mônica", 15, 10.0);
+
+        vetAlunos.adiciona(a1);
+        vetAlunos.adiciona(a2);
+        vetAlunos.adiciona(a3);
+        vetAlunos.adiciona(a4);
+
+        System.out.println("Elementos no vetor: " + vetAlunos.tamanho());
+
+        Aluno alunobusca = new Aluno("Nikola Tesla", 15, 10.0);
+        if (vetAlunos.contem(alunobusca)) {
+            System.out.println("O aluno " + alunobusca.getNome() + " está presente no vetor.");
+        } else {
+            System.out.println("O aluno " + alunobusca.getNome() + " não está presente no vetor.");
+        }
+
+        Aluno alunobusca1 = new Aluno("Ichigo", 17, 4.0);
+        if (vetAlunos.contem(alunobusca1)) {
+            System.out.println("O aluno " + alunobusca1.getNome() + " está presente no vetor.");
+        } else {
+            System.out.println("O aluno " + alunobusca1.getNome() + " não está presente no vetor.");
+        }
+
+     }
+}
+
---
+++ src/Atividade2/Vetor.java
@@ -0,0 +1,36 @@
+package Atividade2;
+
+public class Vetor {
+
+    private final Aluno[] alunos;
+    private int totalAlunos = 0;
+
+
+    public Vetor(int capacidade){
+        this.alunos = new Aluno[capacidade];
+        totalAlunos++;
+    }
+
+    public void adiciona(Aluno aluno) {
+        this.alunos[this.totalAlunos] = aluno;
+        this.totalAlunos++;
+    }
+
+    public int tamanho() {
+        return totalAlunos;
+    }
+
+    public boolean contem(Aluno aluno) {
+        for (int i = 0; i < this.totalAlunos; i++) {
+            if (this.alunos[i] != null && this.alunos[i].equals(aluno)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+}
+
+
+
